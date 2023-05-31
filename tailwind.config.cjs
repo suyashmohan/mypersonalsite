1782b664f48e9e21c4e7f8a5e4f589404e02e52f
Suyash Mohan <suyashatweb@gmail.com>
2023-06-01 02:12:39 +0800

    make it look nice

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        inter: "Inter, sans-serif",
+        sans: ["Oxygen", ...defaultTheme.fontFamily.sans],
       },
     },
   },
